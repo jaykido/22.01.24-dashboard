02bbb1369d034bd77ef68a7dcce95f1f3bec109b
Jay <jaykidokenya@gmail.com>
2024-01-22 19:43:42 +0300

    Start of Project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { CssBaseline } from "@mui/material";
+
+function App() {
+  const globalStyles = {
+    a: {
+      color: "unset",
+      textDecoration: "none",
+    },
+  };
+
+  return (
+    <>
+      <CssBaseline />
+    </>
+  );
+}
+
+export default App;
